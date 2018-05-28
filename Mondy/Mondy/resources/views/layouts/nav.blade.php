d642efb974e0a05bd64db87d5b92eff782d36cd2
naveenperera777 <naveenperera777@gmail.com>
2018-05-29 00:34:37 +0530

    Initial Commit

---
+++ Mondy/Mondy/resources/views/layouts/nav.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="{{ url('/') }}">Mondy</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#toggle-navbar"
+            aria-controls="toggle-navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="toggle-navbar">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="">Dashboard</a>
+                </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">My Courses</a>
+                    </li>
+
+
+        </ul>
+
+        <div class="my-2 my-lg-0">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="">{{ __('Login') }}</a></li>
+                    {{--<li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>--}}
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href=""
+                           onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="" method="POST"
+                              style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+
