af569eac65d0af9f05057451421d82b07c54b1fc
naveenperera777 <naveenperera777@gmail.com>
2018-06-10 13:02:05 +0530

    Fixed Image Looping and Centering Issue

--- Mondy/Mondy/resources/views/dashboards/home.blade.php
+++ Mondy/Mondy/resources/views/dashboards/home.blade.php
@@ -2,26 +2,20 @@
 
 @section('content')<br>
 <div class="container">
-    <div class="row justify-content-center"> 
-        <div class="col-md-4">
-
-            {{-- {{dd($products)}} --}}
-
+    <div class="row text-center"> 
             @foreach($products as $product)
-            
+        <div class="col-md-4">
                 <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="storage/storage/Admin Uploads/Product Uploads/{{$product->attachment}}.{{$product->ext}}" alt="Card image cap">
+                <img class="mx-auto d-block" width="60%" src="storage/storage/Admin Uploads/Product Uploads/{{$product->attachment}}.{{$product->ext}}" alt="Card image cap">
                         <div class="card-body">
                         <h5 class="card-title">{{$product->name}}</h5>
                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                           <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                       </div>
-
-                      @endforeach
-
-
+                   
             </div>
+            @endforeach
         </div>
     </div>
 </div>
